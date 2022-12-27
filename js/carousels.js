68ba20079645191c8fccc270348bd2e0efa75575
imjungim <jungim4201@gmail.com>
2022-12-27 21:58:43 +0900

    🚧 Initiate a new carousel: user-gallery

---
+++ js/carousels.js
@@ -0,0 +1,29 @@
+const productCarousel = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list', //thumbnail buttons
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
+
+const userGallery = tns({
+  container: '.user-gallery .slider-list',
+  controls: false,
+  gutter: 4,
+  edgePadding: 16,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+  responsive: {
+    768: {
+      gutter: 6,
+      edgePadding: 75,
+      controls: true,
+    },
+  },
+})
