21aa8420d1f4766df91a00cdb806f0b31a5a03bf
imjungim <jungim4201@gmail.com>
2023-01-02 16:27:25 +0900

    🚧 Implement deleting individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -5,24 +5,28 @@ const deleteAllButton = gnbSearchHistory.querySelector(
   '.search-history-header button'
 )
 const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
+const deleteButtonList = gnbSearchHistoryList.querySelectorAll('.delete-button')
 
-function closeGnbSearchHistory(e) {
+function closeGnbSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  window.removeEventListener('click', closeGnbSearchHistory)
+}
+
+function closeGnbSearchHistoryOnClickingOutside(e) {
   if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active')
-    window.removeEventListener('click', closeGnbSearchHistory)
+    closeGnbSearchHistory()
   }
 }
 
 //input이 활성화돼서 입력을 할 수 있는상태 -> focus
 function openGnbSearchHistory() {
   //체크 -> gnbSearchHistoryList 안에 li가 몇개? li가 0개 이면 실행 x
-  console.log(gnbSearchHistoryList.children)
   if (gnbSearchHistoryList.children.length === 0) {
     return
   }
 
   if (!gnbSearchHistory.classList.contains('is-active')) {
-    window.addEventListener('click', closeGnbSearchHistory)
+    window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside)
   }
 
   gnbSearchHistory.classList.add('is-active')
@@ -32,7 +36,23 @@ gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
 function deleteAllSearchHistoryItems() {
   //gnbSearchHistoryList 안에 들어있는 li모두 삭제
   gnbSearchHistoryList.innerHTML = ''
-  gnbSearchHistory.classList.remove('is-active')
+  closeGnbSearchHistory()
 }
 
 deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems)
+
+function deleteSearchHistoryItem(e) {
+  e.stopPropagation() //이벤트 전파를 막는 메소드
+
+  //li 삭제
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
+
+  if (gnbSearchHistoryList.children.length === 0) {
+    closeGnbSearchHistory()
+  }
+}
+
+deleteButtonList.forEach((button) => {
+  button.addEventListener('click', deleteSearchHistoryItem)
+})
