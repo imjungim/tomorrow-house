bf76679e7fd07b7315c368e41b73b9c4f0a48d9e
imjungim <jungim4201@gmail.com>
2023-01-02 13:34:38 +0900

    🔇 remove console

--- js/sidebar-drawer.js
+++ js/sidebar-drawer.js
@@ -2,8 +2,6 @@ const drawerMenuButtonList = document.querySelectorAll(
   '.sidebar-nav .drawer-menu-button'
 )
 
-console.log(drawerMenuButtonList)
-
 function toggleDrawerMenu() {
   // drawerMenuButton 을 감싸고 있는 drawer-menu한테 is-open 토글
   const drawerMenu = this.parentNode //부모요소인 drawer-menu를 찾음
